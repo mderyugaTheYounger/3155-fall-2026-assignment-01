f834cd62a75bea25425fac787cb5978d81202fad
mderyugaTheYounger <misha.deryuga+github@gmail.com>
2026-09-02 18:17:14 -0400

    Q13

--- src/exercise13.ts
+++ src/exercise13.ts
@@ -1,5 +1,15 @@
-export type TodoItem = {};
+export type TodoItem = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export async function fetchTodoSafe(todoId: number): Promise<TodoItem | null> {
-  return null;
+  const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+  if(!data.ok){
+    return null;
+  }
+  const ret = data.json();
+  return ret;
 }
