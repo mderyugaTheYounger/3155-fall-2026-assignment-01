b43815a925da484309c703c2fa22360eb2ad9bbd
mderyugaTheYounger <misha.deryuga+github@gmail.com>
2026-09-02 18:00:25 -0400

    Q11

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,8 @@
+import fs from 'fs';
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+  const addition = statusMessage.split('\n').map(line => line + new Date().toISOString()).join('\n') + '\n';
+  await fs.appendFile(filePath, addition, 'utf-8', function(err, data){});
+}
