27c16901380c11a5a099dc85d29951234d8e073c
mderyugaTheYounger <misha.deryuga+github@gmail.com>
2026-09-02 18:30:59 -0400

    Q14

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,15 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+  return Promise.all(postIds.map(async (id) =>{ 
+    const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    if(!data){
+      output.log(`Error with item id ${id}`);
+    }
+    return(await data.json()) as PostItem;
+  }));
 }
