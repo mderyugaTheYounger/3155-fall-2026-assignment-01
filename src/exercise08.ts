158a6867e42e0c3d9191722909dd6bbb4df224f5
mderyugaTheYounger <misha.deryuga+github@gmail.com>
2026-09-02 13:25:19 -0400

    8 was annoying

--- src/exercise08.ts
+++ src/exercise08.ts
@@ -4,10 +4,23 @@ export type EventMap = {
 };
 
 export class SimpleEventEmitter<T extends EventMap> {
+  
+  private listener : {[K in keyof T]?: Array<(data: T[K]) => void>} = {};
+
   public on<K extends keyof T>(
     eventName: K,
     callback: (data: T[K]) => void,
-  ): void {}
+  ): void {
+    if(!this.listener[eventName]){
+      this.listener[eventName] = [];
+    }
+    this.listener[eventName]!.push(callback);
+  }
 
-  public emit<K extends keyof T>(eventName: K, data: T[K]): void {}
+  public emit<K extends keyof T>(eventName: K, data: T[K]): void {
+    const cb = this.listener[eventName];
+    if(cb){
+      cb.forEach((cb) => cb(data));
+    }
+  }
 }
