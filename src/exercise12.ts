208a15e1e2fd07f16ff4921f198617d8a2009be6
mderyugaTheYounger <misha.deryuga+github@gmail.com>
2026-09-02 18:06:01 -0400

    Q12

--- src/exercise12.ts
+++ src/exercise12.ts
@@ -1,3 +1,11 @@
+type RemoteUser = {
+  id: number;
+  name: string;
+  email: string;
+};
+
 export async function fetchUserEmails(): Promise<string[]> {
-  return [];
+  const data = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: RemoteUser[] = await data.json();
+  return users.map((user) => user.email);
 }
