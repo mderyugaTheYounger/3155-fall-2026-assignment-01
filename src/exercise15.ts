b273f01c6dbceadb5b78bb7c4764a30dfcdcd419
mderyugaTheYounger <misha.deryuga+github@gmail.com>
2026-09-02 18:56:51 -0400

    Final commit, finished all but 15

--- src/exercise15.ts
+++ src/exercise15.ts
@@ -1,6 +1,18 @@
+type CommentSummary = {
+  postId: number;
+  id: number;
+  commenterEmail: string;
+};
+
 export async function processCommentsPipeline(
   targetPostId: number,
   outputPath: string,
 ): Promise<number> {
-  return 0;
+  //Step 1
+  const comments = await fetch(`https://jsonplaceholder.typicode.com/posts/${targetPostId}/comments`);
+  //Step 2 + 3
+  const parsedComments = comments.map((comment) => comment.json() as CommentSummary).filter(comment.commenterEmail.endsWith('.org'));
+
+  //Didn't fnish
+  return "whoops";
 }
